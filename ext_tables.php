eb8da3070953cf7da49d0670d9634ac8f60d653a
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-27 16:31:45 +0000

    Renamed default icon and remove obsolete one
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/batchmailer/trunk@73834 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -7,6 +7,8 @@ t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Batch Mailer'
 
 t3lib_extMgm::addLLrefForTCAdescr('tx_batchmailer_domain_model_mail', 'EXT:batchmailer/Resources/Private/Language/locallang_csh_tx_batchmailer_domain_model_mail.xlf');
 t3lib_extMgm::allowTableOnStandardPages('tx_batchmailer_domain_model_mail');
+
+$extensionRelativePath = t3lib_extMgm::extRelPath($_EXTKEY);
 $TCA['tx_batchmailer_domain_model_mail'] = array(
 	'ctrl' => array(
 		'title'	=> 'LLL:EXT:batchmailer/Resources/Private/Language/locallang_db.xlf:tx_batchmailer_domain_model_mail',
@@ -21,9 +23,9 @@ $TCA['tx_batchmailer_domain_model_mail'] = array(
 			'disabled' => 'hidden'
 		),
 		'searchFields' => 'recipients,copies,blind_copies,sender,subject,body',
+		'typeicon_column' => 'sent_status',
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Mail.php',
-		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_batchmailer_domain_model_mail.png'
+		'iconfile' => $extensionRelativePath . 'Resources/Public/Icons/MailDefault.png'
 	),
 );
-
 ?>
